257eec901478c1175da64dafeb894a70302a56cd
Sam Marks <sam@sammarks.me>
2020-07-22 14:10:55 -0400

    feat: switch from serverless -> SAM

--- deploy-to-s3.sh
+++ deploy-to-s3.sh
@@ -1,6 +1,10 @@
-rm -f ./packaged-template.yaml ./sam-template.yaml
+rm -f ./packaged-template.yaml
 
-sls sam export --output ./sam-template.yaml
+cp package.json src/package.json
+cp yarn.lock src/yarn.lock
+cd src
+yarn install --production --frozen-lockfile
+cd ..
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
@@ -20,4 +24,4 @@ aws cloudformation package \
 aws s3 cp ./packaged-template.yaml "s3://sammarks-cf-templates-us-east-2/scheduled-tasks/$PACKAGE_VERSION/template.yaml"
 aws s3 cp ./packaged-template.yaml "s3://sammarks-cf-templates-us-east-2/scheduled-tasks/template.yaml"
 
-rm -rf ./packaged-template.yaml ./sam-template.yaml
+rm -rf ./src/node_modules ./src/package.json ./src/yarn.lock ./packaged-template.yaml
